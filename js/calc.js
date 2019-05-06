f35a9cf552f17ef408147f6edaec7f981965b630
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-06 16:47:23 +0200

    Creating syntax check for duble opperator error

--- js/calc.js
+++ js/calc.js
@@ -1,27 +1,38 @@
-// $(document).ready(function(){
 $(function(){
-	var calc = "";
+	//var calc = "";
+	var calc, input;
+	var opperators = ['-', '+', '/', '*']
+
+	//opperators.indexOf(this.value);
 
 	$("input[type='button']").click(function(){
-		// console.log(this.value);
+		
+		if($.inArray(this.value, opperators) && input == this.value){
+			console.log(input +" = "+ this.value);
+		}
+		//console.log($.inArray(this.value, opperators));
+		//console.log(opperators[5]);
+		// if(opperators.indexOf(this.value)){
+		if($.inArray(this.value, opperators)){
+			//console.log(this.value);
+			
+		}
+		
+		// back function
+
 		if(this.value=="clear"){
-			calc ="";
+			calc =""; // reset calc
 			$("div#total").html(calc);
 		}else{
-
 			calc += this.value;
 			$("div#total").html(calc);
 		}
+		
+		input = this.value;
 	});
 
 	$('input[type="submit"]').click(function(){
-		// console.log(eval(calc));
-		// eval using
-		
-		//if(this.value=="="){
-			calc = eval(calc);
 			$("div#total").html(eval(calc));
-		//}
 	});
 
 });
